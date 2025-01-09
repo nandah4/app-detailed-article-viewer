f37a7bf1ac30bebf0bba3f8ae02ad8c30cb13a92
nandah4 <nyustira6@gmail.com>
2025-01-09 11:15:14 +0700

    fix: add internet permission to resolve image loading issue

--- app/src/main/java/com/example/detailedarticleviewer/CustomAdapterArtikel.kt
+++ app/src/main/java/com/example/detailedarticleviewer/CustomAdapterArtikel.kt
@@ -6,7 +6,6 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.detailedarticleviewer.databinding.ItemRowBinding
-import com.google.android.material.progressindicator.BaseProgressIndicator
 
 class CustomAdapterArtikel(var listArtikel : ArrayList<DataArtikel>) : RecyclerView.Adapter<CustomAdapterArtikel.ListViewHolder>() {
 
